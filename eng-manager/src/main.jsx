a30e470dd4eeaff34e5e2552519ed5cb0c76ffb4
Aniello Heronville <seu-email-do-github@exemplo.com>
2025-12-08 11:00:18 -0400

    Forcar rebuild do site para limpar cache

--- eng-manager/src/main.jsx
+++ eng-manager/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client' // <--- MUDANÇA AQUI
+// MUDANÇA: Importamos apenas a função específica, sem usar "ReactDOM"
+import { createRoot } from 'react-dom/client' 
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-// <--- MUDANÇA AQUI: Removemos "ReactDOM." e usamos direto "createRoot"
+// MUDANÇA: Usamos createRoot direto. Se der erro agora, será outro, não aquele.
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/App_Engenharia">
