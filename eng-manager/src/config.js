67a80d21fb2668869380d525f3a007a84778e637
anielloheronville <78307892+anielloheronville@users.noreply.github.com>
2025-12-10 15:33:58 -0400

    Delete eng-manager directory

--- eng-manager/src/config.js
+++
@@ -1,6 +0,0 @@
-// src/config.js
-export const API_URL = import.meta.env.MODE === 'development'
-  ? 'http://127.0.0.1:8000'             // Usa Localhost se estiver no seu PC
-  : 'https://app-engenharia.onrender.com';    // Usa a URL do Render se estiver no site
-
-export default API_URL;
